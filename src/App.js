2aef47178940eeeac9df63b50853128b6598d77d
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-03-13 23:55:33 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import TerminatorList from "./components/terminator-list/terminator-list.compone
 import SearchBox from "./components/searchbox/searchbox.component";
 
 const App = () => {
+  //models a usestate változó, a setModels a függvény ami módosítja a változó értékét
+  //kezdőértékként egy üres tömböt adok meg
   const [models, setModels] = useState([]);
   const [searchfield, setSearchfield] = useState("");
+  let [count, setCount] = useState(0);
+
+  //console.log(models, searchfield);
 
   const onSearchChange = (event) => {
     //ez a value lesz tárolva a state-ben a searchfield-ben
     setSearchfield(event.target.value);
   };
 
+  //useEffect egyszer mindig lefut amikor újratöltöm a böngésző ablakát
+  //useEffect mindannyiszor meghívódik ahányszor a React újrarendereli a komponenst
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => setModels(users));
+
+    //függőségi lista tömbje. Ha ez a paraméter megvan adva akkor ez egy feltételként működik. Ha ebben a tömbben valami változik akkor a useEffect újra lefut. tehát ha azt akarjuk, hogy a useEffect csak egyszer fusson le, akkor meg kell adni egy üres tömböt
   }, []);
 
   const filteredModels = models.filter((model) => {
@@ -27,8 +36,24 @@ const App = () => {
     return <h1>Betöltés folyamatban...</h1>;
   }
 
+  const up = () => {
+    return setCount(count + 1);
+  };
+  const down = () => {
+    return setCount(count - 1);
+  };
+
+  const reset = () => {
+    return setCount((count = 0));
+  };
+
   return (
     <div className="tc">
+      <h1>Beépített számláló</h1>
+      <h2>{count}</h2>
+      <button onClick={up}>+</button>
+      <button onClick={down}>-</button>
+      <button onClick={reset}>reset</button>
       <h1>Terminator modellek</h1>
       <SearchBox searchBoxProps={onSearchChange} />
       <TerminatorList terminatorListProps={filteredModels} />
